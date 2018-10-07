3e4901951ab8a37bdf7b703cf7a394904a35c5c1
DESKTOP-BCBK69I\Dani <martinthequesionousboy@gmail.com>
2018-10-07 17:33:05 +0200

    181007_1731: Beleraktam a funkcionalitást, hogy a gombok számától függően változzon a gombok rácsos elhelyezkedésének szélessége.

--- PYX.v.0.1.3.py
+++ PYX.v.0.1.3.py
@@ -25,6 +25,9 @@ window_actual_height = window_height_ratio * window_size_factor
 calculated_target = choice(range(1, 10000))
 darab = 0
 
+darabok = MN_darabszam(5)
+
+
 class PyckApp():
     
     def __init__(self):
@@ -45,12 +48,18 @@ class PyckApp():
         self.target = Label(text="The target is: "+str(calculated_target))
         self.target.place(relx=0.5, rely=0.1, anchor=CENTER)
         
-        self.darab = 33 #MN_darabszam(5)
+        self.darab = darabok
+        
+        self.answer = 0
+        if calculated_target % self.darab != 0:
+            self.answer = calculated_target % self.darab
+        else:
+            self.answer = self.darab
         
         self.gombsorszam = Label(text="The number of buttons is: "+str(self.darab)+".")
         self.gombsorszam.place(relx=0.5, rely=0.15, anchor=CENTER)
         
-        self.showing = Button(frame2, text="Testing", command=lambda x: self.answering())
+        self.showing = Button(text="Show me the answer.", command=lambda: self.answering())
         self.showing.place(relx=0.5, rely=0.8, anchor=CENTER)
         
         self.message = Label(text="")
@@ -58,16 +67,35 @@ class PyckApp():
         
         self.solution = Label(text="")
         self.solution.place(relx=0.5, rely=0.2, anchor=CENTER)
-        
-        answer = 0
-        if calculated_target % self.darab != 0:
-            answer = calculated_target % self.darab
-        else:
-            answer = self.darab
+
         
         row = 0
         column = 0
         sorszam = 1
+        
+        if self.darab <= 2**2:
+            margo = 2
+        elif 2**2 < self.darab <= 3**2:
+            margo = 3
+        elif 3**2 < self.darab <= 4**2:
+            margo = 4
+        elif 4**2 < self.darab <= 5**2:
+            margo = 5
+        elif 5**2 < self.darab <= 6**2:
+            margo = 6
+        elif 6**2 < self.darab <= 7**2:
+            margo = 7
+        elif 7**2 < self.darab <= 8**2:
+            margo = 8            
+        elif 8**2 < self.darab <= 9**2:
+            margo = 9
+        elif 9**2 < self.darab <= 10**2:
+            margo = 10
+        elif 10**2 < self.darab <= 11**2:
+            margo = 11
+        else:
+            margo = 12
+        
         for i in range(1, self.darab+1):
             print("Mezo: "+str(i) + ". Sorszama: " + str(sorszam) + ".")
             if i == self.darab:
@@ -81,33 +109,36 @@ class PyckApp():
                 column = 1
                 row = 1
                 sorszam += 1
-            elif 1 < sorszam <= 12:
+            elif 1 < sorszam <= margo:
                 column = sorszam
                 row = 1
                 sorszam += 1
-            elif sorszam % 12 == 0:
-                column = 12
-                row = sorszam // 12
+            elif sorszam % margo == 0:
+                column = margo
+                row = sorszam // margo
                 sorszam += 1
-            elif 12 < sorszam:
-                column = (sorszam % 12) 
-                row = (sorszam // 12) + 1
+            elif margo < sorszam:
+                column = (sorszam % margo) 
+                row = (sorszam // margo) + 1
                 sorszam += 1
             
             button.grid(row=row, column=column)
-        
         self.root.mainloop()
+        
         
     def checking(self, enter):
         if calculated_target % self.darab == enter:
-            print("Congrats, Button "+str(enter)+" was the answer.")
+            #print("Congrats, Button "+str(enter)+" was the answer.")
             self.message["text"] = "Correct answer!"
         else:
-            print("Sorry, Button "+str(enter)+" was not the answer.")
+            #print("Sorry, Button "+str(enter)+" was not the answer.")
             self.message["text"] = "Sorry, the answer was incorrect."
+            
+            
+    def answering(self):
+        #print("Tryna change the answer.")
+        self.solution["text"] = "The answer is "+str(self.answer)+"."
     
-    def answering():
-        self.solution["text"] = "The answer is "+str(answer)+"."
     
 
 Pyck = PyckApp()
